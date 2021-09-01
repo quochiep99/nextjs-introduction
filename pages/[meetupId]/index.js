ad2608dccedd4fe9b869304ea6dd2f52f9dc2b9d
quochiep99 <doanquochiep32@gmail.com>
2021-09-01 22:34:26 +0700

    add custom components

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "./../../components/meetups/MeetupDetail";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
