98baee4fbf0fcc1ed6e442b38e328b7c43e7cb28
quochiep99 <doanquochiep32@gmail.com>
2021-09-04 17:32:08 +0700

    send request to api route

---
+++ pages/api/new-meetup/index.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const newMeetup = req.body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://hiep123:hiep123@cluster0.bepv1.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    await meetupsCollection.insertOne(newMeetup);
+
+    res.status(201).json({ message: "meetup inserted" });
+  }
+};
+export default handler;
